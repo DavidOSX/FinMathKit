727b4883443828fdabf07983bca153cf00a842ac
David Mikheev <53059990+DavidOSX@users.noreply.github.com>
2021-02-10 22:02:04 +0300

    Update IRProvider.cpp

--- IRProvider.cpp
+++ IRProvider.cpp
@@ -8,20 +8,18 @@ IRProvider<IRMode::Const>::IRProvider(char const* a_file) {
         int len = 4, i = 0;
         CcyE cur;
         FILE *f = fopen(a_file,"r");
+    
         for(int j = 0; j < (int)CcyE::N; j++) m_IRs[j] = 0;
+    
         while(fgets(str, len, f)) {
             if(i%2 == 0) {
                 len = 10;
                 cur = Str2CcyE(str);
-            }
-            else {
+            } else {
                 len = 4;
                 m_IRs[(int) cur] = atof(str);
-            }
-            i++;
-        }
-        fclose(f);
-              
+            } i++;
+        } fclose(f);       
 }
     
 
